b88d73b09710d3cfe687082e966cea3467432ba4
Jes Bak Hansen <jeshansen@gmail.com>
2019-09-17 22:45:39 +0200

    Test using 5 values and business logic that does not know that values might be missing

--- Result/Program.cs
+++ Result/Program.cs
@@ -6,7 +6,30 @@ namespace Result
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var gbs1 = new TryFailWithBogus<string>("Lama lama lama lama");
+            var gbs2 = new TryFailWithBogus<string>("alarm, a Lama");
+            var gbb = new TryFailWithBogus<bool>(false);
+            var gbguid = new TryFailWithBogus<Guid>(Guid.NewGuid());
+            var jes = new TryFailWithBogus<decimal>(3.14159m);
+
+            var qss =
+                from s1 in gbs1
+                from s2 in gbs2
+                from b in gbb
+                from id in gbguid
+                from mig in jes
+                select CombineValues(b, s1, s2, id, mig);
+            var result = qss.Resolve(b => b.BogusReason, x => x);
+            Console.WriteLine(result);
+        }
+
+        static string CombineValues(bool printStringLengths, string lamaString, string alarmString, Guid id, decimal pi)
+        {
+            if (printStringLengths)
+            {
+                return (lamaString.Length + alarmString.Length).ToString();
+            }
+            return id + $" {pi}";
         }
     }
 }
